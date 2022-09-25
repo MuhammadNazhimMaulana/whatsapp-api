078b7e0245afa36517cb362a593967cd86cd629c
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-09-25 12:18:47 +0700

    Updating Middleware

--- src/api/middleware/auth.js
+++ src/api/middleware/auth.js
@@ -1,14 +1,26 @@
+// Helper
+const ResponseBulider = require('../helpers/responseBuilder');
+
 const auth = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     // If Auth Header is not exist
     if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err)
+        return ResponseBulider.error(res, 401, 'Anda Belum Terautentikasi'); 
     }
 
+    var auth = new Buffer.from(authHeader.split(' ')[1],
+    'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+ 
+    // If Authorized user
+    if (user == 'admin' && pass == 'password') {
+        next();
+    } else {
+        // If username or password is not the same
+        return ResponseBulider.error(res, 422, 'Username atau Password Salah'); 
+    }
   }
   
   // Exporting modules
